c5d21291ecf53195fb4a7ebbbf86714c49f14143
muhsinmuhsy <muhsinkoodali856@gmail.com>
2023-10-12 19:37:15 +0530

    first commit

--- Base/settings.py
+++ Base/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4ftl-w8613iqzl)q#e66)1o4fnldi=6v7-u#*1h(lku+pz-=ak
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
